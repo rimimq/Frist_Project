54f829bd82e90aecf9e5ba3c91a38da2cc094e53
rimimq <113362828+rimimq@users.noreply.github.com>
2022-12-26 17:47:45 +0900

    Add files via upload

---
+++ curation_animation.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let trigger = new ScrollTrigger({
+        offset:{
+            x:200,
+            y:0
+        },
+        once:true
+    });
+});
